49860ab14d9f685575ef57ccc9d3811109edcedb
josh <josh@joshcurtis.com>
2015-12-30 13:15:47 -0500

    I've been experimentig with the moltin api a little in a standalone html, getting used to the docs and the api. I also started setting up the data in the jewelry module controller to get the ng-repeat divs setup for the jewelry gallery

--- app/jewelry/jewelry.js
+++ app/jewelry/jewelry.js
@@ -1,14 +1,38 @@
 'use strict';
 
-angular.module('myApp.jewelry', ['ngRoute'])
+var myApp_Jewelry_Module = angular.module('myApp.jewelry', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+myApp_Jewelry_Module.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/jewelry', {
     templateUrl: 'jewelry/jewelry.html',
     controller: 'JewelryCtrl'
   });
 }])
 
-.controller('JewelryCtrl', [function() {
+myApp_Jewelry_Module.controller('JewelryCtrl', [function() {
+
+
+	var products = [
+		{
+			name: "bronson",
+			slug: "bronson"
+		},
+		{
+			name: "poppy",
+			slug: "poppy"
+		}
+	];
+
+	console.log(products);
+
+	/*console.log('start');
+	var moltin = new Moltin({publicId: 'xwXTf0jOQhpHRdbYfWpY1pKs4uO7NEGY5J8nq8AV'});
+	moltin.Authenticate(function() {
+  		console.log('authenticated');
+  		// Make your calls here
+  		var products = moltin.Product.List({status:0});
+  		console.log(products[0].slug);
+
+	});*/
 
 }]);
